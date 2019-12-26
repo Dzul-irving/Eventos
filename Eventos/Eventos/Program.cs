71bb0a9a55d69b3578dbe0b1bbed53fceaced814
Dzul-irving <56735292+Dzul-irving@users.noreply.github.com>
2019-12-26 13:16:56 -0600

    Add files via upload

---
+++ Eventos/Eventos/Program.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Eventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> objFecha = new List<string>();
+          try
+            {
+                using (StreamReader objLeer = new StreamReader(@"C:\prueba.txt"))
+                {
+                    
+                    while (!objLeer.EndOfStream)
+                    {
+                        string x = objLeer.ReadLine();
+                        char delimitador = ',';
+                        string[] valores = x.Split(delimitador);
+                        for (int i= 0; i<valores.Length; i++)
+                        {
+                            Console.WriteLine(valores[i]);
+                        }
+
+                        //Console.WriteLine(x);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("El archivo no se puede leer");
+            }
+            Console.ReadKey(); 
+        }
+
+       
+    }
+}
